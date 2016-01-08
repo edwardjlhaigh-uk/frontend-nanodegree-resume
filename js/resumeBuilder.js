ef7833295771666913b5759f41a2052317a509c1
edwardjlhaigh-uk <edward@starlinghaigh.com>
2016-01-08 20:46:40 +0000

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,7 @@
 $("#main").append(["Edward Haigh"]);
 
 var formattedName = HTMLheaderName.replace("%data%", "Edward Haigh");
-$("#main").append([formattedName]);
+$("#header").append([formattedName]);
 
 var formattedRole = HTMLheaderRole.replace("%role%", "Web Developer");
-$("#main").append([formattedRole]);
+$("#header").append([formattedRole]);
