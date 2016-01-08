48086aceb5266fc621c6b537f20fba8aaa740202
edwardjlhaigh-uk <edward@starlinghaigh.com>
2016-01-08 22:36:05 +0000

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,3 +3,14 @@ $("#header").append([formattedName]);
 
 var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
 $("#header").append([formattedRole]);
+
+var bio {
+  name : "Edward Haigh",
+  role : "Web Developer",
+  contact info : "edward@starlinghaigh.com",
+  picture url : "#",
+  welcome message : "Greetings!",
+  skills : "Javascript, jQuery, HTML5, CSS3"
+}
+
+$("#main").append([bio]);
