7b482056a6a957bdda127535a64c8d38ed451d8d
edwardjlhaigh-uk <edward@starlinghaigh.com>
2016-01-08 20:36:18 +0000

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,8 @@
 $("#main").append(["Edward Haigh"]);
 
+var awesomeThoughts = "Things are AWESOME";
+    
+    console.log(awesomeThoughts);
+
 var newThoughts = awesomeThoughts.replace("AWESOME", "FUN");
 console.log(newThoughts);
