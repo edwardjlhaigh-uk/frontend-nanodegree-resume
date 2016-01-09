1211d111055a17176d34183aded7f639ee00f2de
edwardjlhaigh-uk <edward@starlinghaigh.com>
2016-01-09 10:29:43 +0000

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,12 +5,17 @@ var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
 $("#header").append([formattedRole]);
 
 var bio = {
-  name : "Edward Haigh",
-  role : "Web Developer",
-  contact info : "07341 662673",
-  picture url : "#",
-  welcome message : "Greetings!",
-  skills : "Javascript, jQuery, HTML5, CSS3",
+  "name" : "Edward Haigh",
+  "role" : "Web Developer",
+  "contacts" : {
+    "mobile": "07341 662673",
+    "email": "edward@starlinghaigh.com",
+    "github": "edwardjlhaigh-uk",
+    "location": "London"
+  }
+  "welcome message" : "Greetings!",
+  "skills" : ["Javascript", "jQuery", "HTML5", "CSS3"],
+  "bioPic": "images/fry.jpg"
 }
 
 $("#main").append([bio]);
